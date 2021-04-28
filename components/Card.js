d41aaa5b286b4e23234afb50ab87fd791c01164d
youkoucoding <youkousai@gmail.com>
2021-04-28 22:40:03 +0900

    Add Card & dependencies

---
+++ components/Card.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className='mt-16 relative max-w-4xl mx-auto'>
+      <div
+        className="absolute z-10 inset-0 bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-lg"
+      ></div>
+      <div class="relative z-20 rouded-md shadow-md bg-white p-8">
+        <img
+          src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2552&q=80"
+          alt="#"
+          class="h-80 w-full object-cover"
+        />
+        <div class="p-8">
+          <h4 class="font-bold text-2xl">Work Title</h4>
+          <p class="leading-normal pt-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi,
+            eaque quaerat. Hic quasi dicta quo! Incidunt molestias
+            repudiandae voluptatum beatae alias? Dolores perspiciatis
+            eligendi voluptatibus pariatur corporis tempore fuga voluptas.
+              </p>
+          <a
+            href="#"
+            class="inline-block bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 shadow-lg px-6 py-2 rounded-md text-white text-sm mt-4 font-bold"
+          >View More</a
+          >
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
